3a04c0970fbcfbeef36339ac226bb9d12839f0e7
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-29 12:26:51 +0100

    Update postRoutes.js

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -4,17 +4,15 @@ const User=require('../ModelSchema/UserSchema')
 const bcrypt=require('bcrypt')
 const middleware=require('../middleware')
 
-router.get('/:postID',middleware.requireLogin,(req,res,next)=>{  //better to have middleware MW checking user session valid?
+router.get('/:postID',middleware.requireLogin,(req,res,next)=>{  
     var payload={
         pageTitle:'View post',
-        userLoggedIn_post:req.session.user,  //similar to GET '/' rendering home pagein app.js (getAllPostsData in home.js on page loading using postsAPI)
-        userLoggedInJs_post:JSON.stringify(req.session.user), ////similar to GET '/' rendering home pagein app.js; same as above
-        postID:`${req.params.postID}` //or just req.params.postID direcly (as below two console o/p same type & value)
-        //to postPage.pug->postPage.js getting data on page loading using postsAPI
+        userLoggedIn_post:req.session.user,  
+        userLoggedInJs_post:JSON.stringify(req.session.user), 
+        postID:`${req.params.postID}` //or just req.params.postID 
     }
-    console.log('params',req.params.postID,typeof req.params.postID) //string
-    console.log("postID",payload.postID,typeof payload.postID) //string same as above
-    res.status(200).render('postPage',payload) //webViews folder 'postPage.pug' for this GET /:postID
+    
+    res.status(200).render('postPage',payload)
 })
 
 module.exports=router;
