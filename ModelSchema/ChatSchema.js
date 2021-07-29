7075f9ce558a89e5341154ac570583f4423d85e2
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-29 12:12:07 +0100

    Update ChatSchema.js

--- ModelSchema/ChatSchema.js
+++ ModelSchema/ChatSchema.js
@@ -6,11 +6,7 @@ const chatSchema=new Schema({
     chatName:{type: String,trim:true},
     isGroupChat:{type:Boolean},//only one; wont add other users
     users:[{type:Schema.Types.ObjectId,ref:"User"}],
-    latestMessage:{type:Schema.Types.ObjectId,ref:"Message"} 
-    //what the latest Msg was + which chats were teh last ones to be modified
-    //the chat list is ordered by the most recently active chat first (otherwise ordered by the details of another collection)
-    //send Msg-> added to Msg collection; not the chat collection we're creating here so chat collection wont be affected
-    //keep track of latestMsg, timestamp also updated then ordered by the most recently updated 
+    latestMessage:{type:Schema.Types.ObjectId,ref:"Message"}  
 
 },{timestamps:true})
 
