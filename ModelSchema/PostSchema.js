66959f869281a8293043a97b38041bc149dc045e
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-29 12:15:33 +0100

    Update PostSchema.js

--- ModelSchema/PostSchema.js
+++ ModelSchema/PostSchema.js
@@ -3,12 +3,12 @@ const mongoose=require('mongoose')
 const Schema=mongoose.Schema;
 
 const PostSchema=new Schema({
-    content:{ type:String,trim:true}, //not required when retweet
+    content:{ type:String,trim:true}, 
     postedBy:{type:Schema.Types.ObjectId,ref:"User"},
     pinned:Boolean,
     likes:[{type:Schema.Types.ObjectId,ref:"User"}],
     retweetUsers:[{type:Schema.Types.ObjectId,ref:"User"}],  //follower //like likes
-    retweetData:{type:Schema.Types.ObjectId,ref:"Post"}, //should be retweetedData/Post //parentPost //like content
+    retweetData:{type:Schema.Types.ObjectId,ref:"Post"}, //should be retweetedData/Post //parentPost
     commentedPost:{type:Schema.Types.ObjectId,ref:"Post"} //parentPost
 },{timestamps:true})
 
