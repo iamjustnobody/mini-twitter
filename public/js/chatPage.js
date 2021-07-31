2cbe1faf5fd9e96e7f9afca3ba659459347dd654
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-31 23:50:11 +0100

    Update chatPage.js

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,59 +1,27 @@
-/*
-console.log(chatID1,typeof chatID1) //string
-console.log(chatID2,typeof chatID2) //string
-console.log(chatID3,typeof chatID3) //string
-console.log(chatID4,typeof chatID4) //string
-//console.log(chatID5,typeof chatID5)
-//console.log(chatID6,typeof chatID6)
-//console.log(chatID7,typeof chatID7)
-//console.log(chatID8,typeof chatID8)
-console.log(document.querySelector('.test').dataset.id1,typeof document.querySelector('.test').dataset.id1)//string
-console.log(document.querySelector('.test').dataset.id2,typeof document.querySelector('.test').dataset.id2)//string
-console.log(document.querySelector('.test').dataset.id3,typeof document.querySelector('.test').dataset.id3)//string
-console.log(document.querySelector('.test').dataset.id4,typeof document.querySelector('.test').dataset.id4)//string
-*/
 
 
 $("document").ready(()=>{
 
-    socket.emit("join room",chatID) //emit an event to the server
-    socket.on("typing",()=>$('.typingDots').show()) //server app.js emit typing event
-    socket.on("stop typing",()=>{$('.typingDots').hide();console.log('is typing')}) //server app.js emit typing event
+    socket.emit("join room",chatID) 
+    socket.on("typing",()=>$('.typingDots').show()) 
+    socket.on("stop typing",()=>{$('.typingDots').hide()}) 
     
-    $.get(`/api/chats/${chatID}`,(chatData)=>{ console.log(chatData,typeof chatData) //chatArrayObj - inside users field populated
+    $.get(`/api/chats/${chatID}`,(chatData)=>{ 
         $("#chatName").text(chatData.chatName?chatData.chatName:getOtherChatUsersNamesString(chatData.users))
-        //copied from inboxPage.Js
-        //above helperfunctions in here chatPageJs (retuned data/argument from apiRoute chatsJs)
-        //below:moving helper functions to apiRoute chatsJs '/api/chats/chatid'
-        //$("#chatName").text(chatData.chat_name) // ok when get('/:chatid') from chatsJs return plainJsObj1     
-        //$("#chatName").text(chatData.chatName) // ok when get('/:chatid') from chatsJs return plainJsObj2 or chat
-        //$("#chatName").text(chatData.chat_name) //not ok newly created field on mongodbObj only works in backend;this field getting lost in frontendJs
-    }) //ok
-    
-    //$("#chatName").text(chatJs.chatName?chatJs.chatName:getOtherChatUsersNamesString(chatJs.users)) //ok too no need to make ajax call 
-    //just pass stringified chatObj from messagesRoutes GET '/messages/chatid' payload to chatPage.pug
-    //helperfunctio in chatPageJs; no need for apiRoute chatsJs; data/argument from non-apiRoute messagesRouteJs
-
+        
+    }) 
 
 
 
     //OUTPUT ALL CHAT_CONTETN/CHAT_MESSAGES IN THIS CHAT
     $.get(`/api/chats/${chatID}/messages`,(chatMsg)=>{ 
-        console.log('all messages for this chat',chatMsg,typeof chatMsg) //arrayObj - could be length of 0 if new chat; inside-content-obj see chatMsg[index] below
-        //$("#chatName").text(chatMsg[0].chat.chatName?chatMsg[0].chat.chatName:getOtherChatUsersNamesString(chatMsg[0].chat.users)) //OK
-        //populate users in router.get('/:chatid/messages') at chats.js so above $.get(`/api/chats/${chatID}` can be removed
 
         var messages=[]
         var lastSenderId=""
 
         chatMsg.forEach((message,index)=>{
-            console.log(chatMsg[index+1],typeof chatMsg[index+1]) //{populatedChat(populated chat.users),seenBy,populatedSender} "object";undefined "undefined"
 
-        //    var html=createMessageHtml(message) //stirng (not jqueryObj)
-            //var html=createAdvancedMessageHtml(message,chatMsg[index+1],lastSenderId) //ok//what if index==chatMsg.length-1? chatMsg[index+1] is null?
-            var html=createAdvancedMessageHtml(message,chatMsg[index+1],lastSenderId) //or index<chatMsg.length-1?chatMsg[index+1]:'undefined'
-            //chatMsg[index+1]:"undefined" or chatMsg[index+1]:undefined or chatMsg[index+1]:null //see notebook            
-            //if(index<chatMsg.length-1){console.log(typeof chatMsg[index+1].sender._id)}//string
+            var html=createAdvancedMessageHtml(message,chatMsg[index+1],lastSenderId) 
 
             messages.push(html) //1
             //$('.chatMessages').append(html) //normal version //2a
@@ -61,22 +29,17 @@ $("document").ready(()=>{
 
             lastSenderId=message.sender._id //sender is populated
         })
-        var messagesHtml=messages.join('') //one big string //1
+        var messagesHtml=messages.join('') //1
 
         //$('.chatMessages').append(messagesHtml) //normal version //1a
         addMessagesHtmlToPage(messagesHtml,false) //advanced version //1b
         //replacing messages.push with either one of the above two (aim to append) and thus remove messages.join? ok solution 1& 2
     
         $(".loadingSpinnerContainer").remove()
-        $('.chatContainer').css("visibility",'visible') //can also be applied to other pages
+        $('.chatContainer').css("visibility",'visible') 
+    }) 
 
-        //markAllMessagesAsRead()//or outside ajax call
-        //fire PUT api call (inside GET-apiReq) marking all msg in this chat as read/seen when (clicking one of chats in inboxPage or) open chatPage (onloading)
-        //incl refreshChatBadges
-
-    }) //populate users in router.get('/:chatid/messages') at chats.js so above $.get(`/api/chats/${chatID}` can be removed
-
-    markAllMessagesAsRead() //or here or inside the ajax calls above
+    markAllMessagesAsRead() 
 
 }) //IF NO OUTPUT ALL CHATMESSAGES, 
 //then documentReady above $.get(`/api/chats/${chatID}` can be replaced by defining default name or chat name in messagesRoute '/messages/chatid'(with helper functions in messagesRoutesJs)
@@ -90,23 +53,14 @@ function addMessagesHtmlToPage(html,animated){
 }
 function createAdvancedMessageHtml(message,nextMessage,lastSenderId){
 
-    //console.log('lastSenderId',typeof lastSenderId) //string
     var currentSender=message.sender; 
-    var curSenderName=currentSender.fName+" "+currentSender.lName //'sender' is populated
-    var curSenderId=currentSender._id //'sender' is populated
-    //console.log('currentSenderId',typeof curSenderId) //string
+    var curSenderName=currentSender.fName+" "+currentSender.lName 
+    var curSenderId=currentSender._id 
 
-    console.log('nextMessage',nextMessage,typeof nextMessage) //{populatedChat(populated chat.users),seenBy,populatedSender} object or undefined undefined or null obj
-    //getallmessagesofthissinglechat (GET on document Ready):object;or undefined undefined when index==chatMsg.length-1
-    //POST send a new message: null obj if middle argument passed in is null, or undefined undefined
-    var nextSenderId=nextMessage!=undefined?nextMessage.sender._id:"" //!=null or !=undefined or !='undefined' see notebook
-    //'sender' is populated
-    console.log('nextSenderId',nextSenderId,typeof nextSenderId) //string; if nextMessage undefined if index==chatMsg.length-1, nextSenderId empty string
-    
+    var nextSenderId=nextMessage!=undefined?nextMessage.sender._id:""  
 
     var isFirst=lastSenderId!=curSenderId
-    var isLast=curSenderId!=nextSenderId //true if index+1==chatMsg.length (last)
-
+    var isLast=curSenderId!=nextSenderId 
     var isMine=message.sender._id==userLoggedInJs._id 
     var liClassName=isMine?"mine":"theirs"
 
@@ -119,18 +73,17 @@ function createAdvancedMessageHtml(message,nextMessage,lastSenderId){
     }
 
     var profileImage=''
-    if(isLast){ //can be both first & last if only one
+    if(isLast){ 
         liClassName+=" last"
         profileImage=`<img src='${currentSender.profilePic}'>`
     }
-    //if nor first or last, then inthe middle
 
      var imageContainer=""
      if(!isMine){
         imageContainer=`<div class='imageContainer'>
                             ${profileImage}
                         </div>`
-     } //as only as its not my msg, even if its not the last msg we'll keep imageContainer space
+     }
 
     
     return `<li class='message ${liClassName}'>
@@ -151,28 +104,23 @@ function createAdvancedMessageHtml(message,nextMessage,lastSenderId){
 
 $('#chatNameButton').click(()=>{
     var name=$('#chatNameTextbox').val().trim();
-    //console.log(name)
+
     $.ajax({
         url:'/api/chats/'+ chatID, 
         type:'PUT',
         data:{
             chatname:name
         },
-        success:(updatedData,status,xhr)=>{ //returned updatedChat from PUT /api/chats/chatid PUT from chatsJs
+        success:(updatedData,status,xhr)=>{ 
             if(xhr.status!=204){
                 //return alert('could not update')
             }
             location.reload();
-            //$("#chatName").text(updatedData.chatName?updatedData.chatName:getOtherChatUsersNamesString(updatedData.users))
-            //above needs returned updatedChat (populated) from api route PUT '/api/chats/chatid" from chatsJs; also need to hide modal
-            //ok as only has one input chatname field/bar on the chatPage.pug so with unique id (not using css class queryselector)
-            //(so above html/text change wont apply to other with same classname like pin/unpin in commonJs) - list of post items
-            //could solve by queryselector[index] (or specifying/adding another extra css class - but cannot do so for list of items which has same class but now diff classes just like ids)
-        }
+       }
     })
 })
 
-//below two copied from inboxPage.js; or remove dup codes from both Js & move to commonJs instead
+
 function getOtherChatUsersNamesString(users){
     var otherChatUsers=getOtherChatUsers(users);
     var namesArray=otherChatUsers.map(user=>{return user.fName+" "+user.lName})
@@ -190,36 +138,33 @@ function getOtherChatUsers(users){
 
 //send Messages
 $('.sendMessageButton').click(()=>{
-    messageSubmitted() //refreshMsgBadge or markAllMsgasRead in here click eventHandler or inside  msgSubmitted or sendMessage
+    messageSubmitted() 
 })
 $('.inputTextbox').keydown((event)=>{
 
     updateTyping();
 
     if(event.which===13 && !event.shiftKey){
-        messageSubmitted() //enter key for submission
-        return false //prevent entering new line
+        messageSubmitted() 
+        return false 
     }
 })
 var typing=false;
 var lastTypingTime;
 function updateTyping(){
     if(!connected){return}
-    //below for keydown - going to type
     if(!typing){
         typing=true;
-        socket.emit('typing',chatID) //to server app.js
+        socket.emit('typing',chatID) 
     }
     
-    //below is like alread typed & done typing; keyUp when/if(typing===true)
+    
     lastTypingTime=new Date().getTime()
     var timeLength=3000
     setTimeout(()=>{
         var timeDiff=new Date().getTime()-lastTypingTime
         if(timeDiff>=timeLength && typing){
-            //for serial chars typing in, prev cb maynot execute as newTime (later's Data.now()) makes timeDiff<timeLength; 
-            //later cb's typing may be false as prev's typing is set to false in prev's cb fn; so later's cb may not need to execute
-            socket.emit('stop typing',chatID) //to server appjs
+            socket.emit('stop typing',chatID) 
             typing=false
         }
     },timeLength)
@@ -228,9 +173,9 @@ function updateTyping(){
 function messageSubmitted(){
     var content=$('.inputTextbox').val().trim();
     if(content!=""){
-        sendMessage(content) //refreshMsgBadge or markAllMsgasRead in here msgSubmitted or inside sendMessage
+        sendMessage(content) 
         $('.inputTextbox').val("")
-        socket.emit('stop typing',chatID) //to server appjs
+        socket.emit('stop typing',chatID) 
         typing=false
     }
 
@@ -242,58 +187,33 @@ function sendMessage(content){
             $('.inputTextbox').val(content)
             return;
         }
-        console.log("newMsg",newMsg) //still shows seenBy empty array as its just sent noOne read yet
-        //POST@messagesJs-apiRoutes: when create, seenBy not specified as newMessage field/value
-        //but will output seenBy ARRAY field in frontend console & show in mongodb
-        //but when create a new chat mongodb does not show the fields thatare not defined or specified in chatData when create/POST @chatsJs apiRoutes
-        //newMsg obj {populatedChat(populated chat.users),populatedSender}
 
-        //populated sender & chat from messagesJs apiRoute; consistent with ouputChatMessages populated by GET('/api/chats/:chatid/messages')from chats.js
-        addChatMessageHtml(newMsg) //for self seeing my own newMsg thats just sent
-        /*
-        console.log('new message sent back to backend server appJs', newMsg.chat,newMsg.chat.users) 
-        // if chat.users not populated//{users:["x","y"],_id:"z",lastMessage:"u",updatedAt:"time"} //["x","y"]
-        //if chat.users populated: obj {_id:"x",isgroup:true,updatedAt:"time",users:[{obj see below}]}
-        //if chat.users populated: ArrayObj [{_id:"x",createdAt:"time",likes:["id1","id2"]},{see left}]
-        console.log(typeof newMsg.chat,typeof newMsg.chat.users) //obj; arrayObj of string b4 populated //obj;arrayObj of oBj after populated
-        console.log('chatid',newMsg.chat._id,typeof newMsg.chat._id,newMsg.chat.id,typeof newMsg.chat.id) //z string undefined undefined
-        console.log('userobj',newMsg.chat.users[0],typeof newMsg.chat.users[0]) //x or y string b4 populated
-        //{_id:"x",createdAt:"time",likes:["id1","id2"]} obj after chat.users being populated
-        console.log('userid',newMsg.chat.users[0]._id,typeof newMsg.chat.users[0]._id,newMsg.chat.users[0].id,typeof newMsg.chat.users[0].id) //all4undefined
-        ////z string undefined undefined after chat.users populated
-        console.log('senderobj',newMsg.sender,typeof newMsg.sender) //{_id:"x",createdAt:"time",likes:["id1","id2"]} obj //sender's being populated
-        console.log('senderid',newMsg.sender._id,typeof newMsg.sender._id,newMsg.sender.id,typeof newMsg.sender.id)//z string undefined undefined 
-        //see difference in consol o/p of newMsg in messagesJs chatPageJs appJs
-        */
-       console.log('start emitting from chatPageJs')
+        addChatMessageHtml(newMsg) 
         if(connected) socket.emit('new message',newMsg) //1b&2 //for others seeing my newMsg thats just sent //1b,2
        //const chatMsg={newMsg,chatID};if(connected) socket.emit('new message',chatMsg) //1a, 1a2
         //markAllMessagesAsRead() //or placed in click sendMessageButton or messageSubmitted fn or addchatmsghtml fn below
-        console.log("refreshChatsBadge from sendMessage in chatPage frontJs onload"); //refreshChatsBadge() //or placed in clickSendMsgbtn/msgSubmitted/addchatmsghtml
+         //refreshChatsBadge() //or placed in clickSendMsgbtn/msgSubmitted/addchatmsghtml
     })
 }
 
 function addChatMessageHtml(message){
-    if(!message||!message._id){ //if message is not populated;however it's def populated as its return as whole obj not just IDObj
+    if(!message||!message._id){ 
         alert("Message is not valid")
         return
     }
     ///var messageDiv=createMessageHtml(message) //normal version
-    var messageDiv=createAdvancedMessageHtml(message,null,'') //advanced version //see notebook: null (null Obj) or undefined (undefined undefined) or "undefined" (undefined string)
+    var messageDiv=createAdvancedMessageHtml(message,null,'') //advanced version //see notebook: 
     //$('.chatMessages').append(messageDiv) //normal version
     addMessagesHtmlToPage(messageDiv,true) //advanced version
 
     console.log("refreshChatsBadge from sendMessage in chatPage frontJs addhtml"); //refreshChatsBadge()
-    markAllMessagesAsRead() //still in above POST req cb fn - cb executing after successfully posting/sendign msg to mongodb
-    //or placed outside fn see above click sendMessageButton or messageSubmitted or sendmessage fn
+    markAllMessagesAsRead() 
 }
 
 function createMessageHtml(message){
 
-    var isMine=message.sender._id==userLoggedInJs._id //message.sender is just string so need to be populated@messages.js
+    var isMine=message.sender._id==userLoggedInJs._id 
     var liClassName=isMine?"mine":"theirs"
-    //<li class='message ${liClassName}'> //<li class=${liClassName}> //<li class=${isMine?"mine":"theirs"}>
-    ////populated sender & chat from messagesJs apiRoute //consistent w ouputChatMessages populated by GET('/api/chats/:chatid/messages')from chats.js
     return `<li class='message ${isMine?"mine":"theirs"}'>
                 <div class='messageContainer'>
                     <span class='messageBody'>
@@ -306,9 +226,9 @@ function createMessageHtml(message){
 
 function scrollToButtom(animated){
     var container=$('.chatMessages')
-    var scrollHeight=container[0].scrollHeight //JS obj.Js property
+    var scrollHeight=container[0].scrollHeight 
     if(animated){
-        container.animate({scrollTop:scrollHeight},'slow') //could specify milisec
+        container.animate({scrollTop:scrollHeight},'slow') 
     }
     else{
         container.scrollTop(scrollHeight)
